bf10482fdebbb7caa59df3c04edeed3d44bf7035
eggyyuliandika <eggyyuliandika@gmail.com>
2024-01-23 01:32:46 +0800

    add db

---
+++ db.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: 1,
+    username: "Eggyyuliandika",
+    email: "eggyyuliandika@gmail.com",
+    password: "apaaja",
+  },
+  {
+    id: 2,
+    username: "Putri",
+    email: "putriwulandari@gmail.com",
+    password: "siapaja",
+  },
+];
+
+module.exports = { users };
