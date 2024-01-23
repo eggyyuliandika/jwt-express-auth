08ceb984f4d0a05a238fd72b6822d2b34f0844aa
eggyyuliandika <eggyyuliandika@gmail.com>
2024-01-23 20:37:01 +0800

    check the authorization in header

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,4 +2,9 @@ const { decode } = require("../utils/jwt");
 
 function authorization(req, res, next) {
   const { authorization = null } = req.headers;
+
+   
+  if(!authorization) {
+    return res.status(403).send({error: "No authorization header found"})
+  }
 }
